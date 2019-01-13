0501af20776f968cee581bb0bee2a0bed2100eba
etwyt <eutinawei@gmail.com>
2019-01-13 12:01:32 +0800

    html css js file

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+HERE = os.path.dirname(os.path.abspath(__file__))
+HERE = os.path.join(HERE, '../')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HERE, 'static/'),
+)
--- singer/views.py
+++ singer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Song
+from .models import Style
+from .models import Performance
+from .models import Vocalist
+from .models import Instrumentalist
 
 # Create your views here.
+
+def home(request):
+    Song_list = Song.objects.all()
+    return render(request, 'home.html', {'Song_list': Song_list,})
